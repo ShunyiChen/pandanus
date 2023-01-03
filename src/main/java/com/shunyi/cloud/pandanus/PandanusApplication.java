923e383c5c83de26c90483b5cb035bcda4575993
Simeon <simeon.chen@gds.ey.com>
2023-01-03 09:55:13 +0800

    Create source code

---
+++ src/main/java/com/shunyi/cloud/pandanus/PandanusApplication.java
@@ -0,0 +1,13 @@
+package com.shunyi.cloud.pandanus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandanusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandanusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shunyi/cloud/pandanus/PandanusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shunyi.cloud.pandanus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandanusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
